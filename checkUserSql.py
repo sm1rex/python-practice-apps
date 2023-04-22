7200963d24d1d8ef1d15125ad369b4066359dbce
Maksym Smirnov <45522388+sm1rex@users.noreply.github.com>
2023-04-22 09:58:57 +0300

    Create checkUserSql.py

---
+++ checkUserSql.py
@@ -0,0 +1,34 @@
+import sqlite3
+from aiogram import types, executor, Dispatcher, Bot
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import *
+
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute(
+    ''' CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL)  ''')
+conn.commit()
+
+bot = Bot(token='6188787794:AAFj5a0fZqRmIN8Y_I6y5xiDr03ziiPRK5Q')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.full_name
+
+    cursor = conn.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+    user = cursor.fetchone()
+    if user is not None:
+        await bot.send_message(message.chat.id, "You registered yet.")
+    else:
+        cursor.execute(
+            "INSERT INTO users (id, name) VALUES (?, ?)", (user_id, user_name))
+        conn.commit()
+        await bot.send_message(message.chat.id, "WELCOME TO MY BOT)")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
